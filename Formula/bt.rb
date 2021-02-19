062ca199796f656c776cefda5299b9e79c18ec26
Jack Thorne <jack@kilmer.co>
2021-02-19 11:26:22 -0600

    version bump to 0.5.3

--- Formula/bt.rb
+++ Formula/bt.rb
@@ -1,10 +1,10 @@
 class Bt < Formula
   desc "Tools for interacting with the Braintree API"
   homepage "https://github.com/wontruefree/braintree"
-  url "https://github.com/wontruefree/braintree/archive/v0.5.2.tar.gz"
-  sha256 "7b3a7c68bae4fb4262817942b5184d5158e30f4426092cafbfc47c8faf9cf1b2"
+  url "https://github.com/wontruefree/braintree/archive/v0.5.3.tar.gz"
+  sha256 "fa11451ed0e10a838c7f58fc2f2f51b74a8a2907c456a432c82bc70d4841f886"
   license "MIT"
-  version "0.5.2"
+  version "0.5.3"
   depends_on "crystal"
 
   bottle :unneeded
